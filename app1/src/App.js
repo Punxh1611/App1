d8732130b533ee0db4dbcaec2daf53cd69ee79f4
Punch <120019653+Punxh1611@users.noreply.github.com>
2026-07-13 11:18:32 +0700

    add Style.css + externel css

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,18 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
 function App() {
-  const divStyle = {
-    color: 'red',
-    backgroundColor: 'powderblue',
-    fontSize: 'larger',
-    padding: '3px',
-  };
   return (
-    <div style={divStyle}>
-      <h3>Hello React</h3>
-    </div>
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className="title">React & React Native</div>
+    </>
   );
 }
-
 export default App;
